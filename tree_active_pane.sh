2d833793db17a961f0a808e9d3ba33bd159d0dc6
Chengran <cli728@uwo.ca>
2021-10-28 10:57:22 -0400

    add comments

--- tree_active_pane.sh
+++ tree_active_pane.sh
@@ -5,12 +5,17 @@ function pwdx {
 }
 
 function tree_active_pane () {
+  # get the active pane's pid and wd
   active_pane_pid=$(tmux list-panes -F '#{pane_active} #{pane_pid}' | grep -o '^1[[:space:]][0-9]\+$' | grep -o '\w[0-9]\+')
   active_wd=$(pwdx $active_pane_pid)
+  # 2 layer tree view of file with color
   tree -LC 2 $active_wd
 }
 
 export -f pwdx
 export -f tree_active_pane
 
-watch -tcn 1 tree_active_pane
+function sidebar() {
+
+  watch -tcn 1 tree_active_pane
+}
