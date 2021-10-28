25c5352a41a338f5d222cfd67ba3a4ea6ddd261d
Chengran <cli728@uwo.ca>
2021-10-28 06:49:20 -0400

    first version of working active_track_pane

---
+++ tree_active_pane.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function pwdx {
+  lsof -a -d cwd -p $1 -n -Fn | awk '/^n/ {print substr($0,2)}'
+}
+
+function tree_active_pane () {
+  active_pane_pid=$(tmux list-panes -F '#{pane_active} #{pane_pid}' | grep -o '^1[[:space:]][0-9]\+$' | grep -o '\w[0-9]\+')
+  active_wd=$(pwdx $active_pane_pid)
+  tree -LC 2 $active_wd
+}
+
+export -f pwdx
+export -f tree_active_pane
+
+watch -tcn 1 tree_active_pane
