e393fe8f34769843515e1ed00c53cee12cf733a4
ChengranAA <andrew_lcr@icloud.com>
2021-11-09 15:50:47 -0500

    small changes on comment and readme

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 A sidebar tool for displaying files in current woring directory based on watch and tree command line tools. 
 
+Must intall tree and watch command first.
+
 ![demo](demo.gif)
 
--- tree_active_pane.sh
+++ tree_active_pane.sh
@@ -9,9 +9,10 @@ function tree_active_pane () {
   active_pane_pid=$(tmux list-panes -F '#{pane_active} #{pane_pid}' | grep -o '^1[[:space:]][0-9]\+$' | grep -o '\w[0-9]\+')
   active_wd=$(pwdx $active_pane_pid)
   #list file tree with color and 2 layer
-  tree -LC 2 $active_wd
+  tree -LC 1 $active_wd
 }
 
+# allow subprocess to access the function 
 export -f pwdx
 export -f tree_active_pane
 
